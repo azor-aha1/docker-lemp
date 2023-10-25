673d2426051a235099edf509d900c8d961775a07
valmar <vovatheraven@gmail.com>
2023-10-25 19:45:10 +0700

    fix root folder for index.php; pdo_mysql added test db script added

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'started';
+$dsn = 'mysql:dbname=demo;host=mysql';
+$user = 'root';
+$password = '123';
+$pdo = new PDO($dsn, $user, $password);
+$stmt = $pdo->query('show databases;');
+print_r($stmt->fetchAll(PDO::FETCH_OBJ));
+echo 123;
